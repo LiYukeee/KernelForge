d81517ac4f8923338688f4c1ad32f5bd6095ff28
LiYuke <liyukee@qq.com>
2026-07-08 18:52:04 +0800

    v0.5.1 Change default python

--- scripts/env.sh
+++ scripts/env.sh
@@ -8,7 +8,7 @@ if command -v nvidia-smi >/dev/null 2>&1; then
   export PYTHON_BIN=/home/liyk/miniconda3/envs/llm/bin/python
 elif command -v mx-smi >/dev/null 2>&1; then
   export MACA_HOME=/opt/maca-3.3.0
-  export PYTHON_BIN=/home/yuke/miniforge-pypy3/envs/mxtorch/bin/python
+  export PYTHON_BIN=/home/yuke/miniforge-pypy3/envs/torch_new/bin/python
 else
   echo "Neither nvidia-smi nor mx-smi was found in PATH." >&2
   return 1 2>/dev/null || exit 1
