d7f39c6b2682a2e536c7218c3abe493850e86383
LiYuke <liyukee@qq.com>
2026-08-10 14:36:25 +0800

    update env

--- scripts/env.sh
+++ scripts/env.sh
@@ -9,6 +9,7 @@ if command -v nvidia-smi >/dev/null 2>&1; then
 elif command -v mx-smi >/dev/null 2>&1; then
   export MACA_HOME=/opt/maca-3.3.0
   export PYTHON_BIN=/home/yuke/miniforge-pypy3/envs/torch_new/bin/python
+  export TORCH_CUDA_ARCH_LIST="8.0"
 else
   echo "Neither nvidia-smi nor mx-smi was found in PATH." >&2
   return 1 2>/dev/null || exit 1
